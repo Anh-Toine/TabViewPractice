d4a9353b9de7f70ba9832fd23230b71d02c2095c
Anh-Toine <52642269+Anh-Toine@users.noreply.github.com>
2022-09-30 10:22:56 -0400

    Initial Commit

---
+++ TabViewPractice/TabViewPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewPracticeApp.swift
+//  TabViewPractice
+//
+//  Created by Lukas Nguyen on 2022-09-30.
+//
+
+import SwiftUI
+
+@main
+struct TabViewPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
